c3853f72464046f201ec1d2b7fb85b3f5bed403f
nopall .here <nauval.cool.28@gmail.com>
2026-07-21 19:17:41 +0700

    update dashboard

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -66,11 +66,11 @@
             </div>
 
             <!-- Stok Produk & Statistik per minggu -->
-            <div class="flex border-neutral-200 dark:border-neutral-700 p-1 gap-1">
+            <div class="flex border-neutral-200 dark:border-neutral-700 p-2 gap-4">
                 <!-- Statistik per minggu -->
-                <flux:card class="space-y-4 h-full dark:text-white" style="width: 66%; height: 330px;" align="center">
+                <flux:card class="space-y-2 h-full dark:text-white" style="width: 66%; height: 330px;" align="center">
                     {{-- Header card --}}
-                    <div class="flex-1 rounded-xl">
+                    <div class="flex rounded-xl justify-between items-center p-1">
                         <div class="m-10 bg-white dark:bg-white-800 dark:text-white rounded shadow"
                             style="text-color:white; align-items: center;">
                             {!! $chart->container() !!}
